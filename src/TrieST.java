971273ca39a809ed0f304f63db3b6beb9b218c04
theGaryLarson <gary.larson316@gmail.com>
2022-11-27 00:13:09 -0800

    Generifying the author's example of Trie class

--- src/TrieST.java
+++ src/TrieST.java
@@ -1,34 +1,46 @@
 import java.util.Arrays;
-public class TrieST<Value>
+
+public class TrieST<K>
 {
-    private static final int R = 256; // radix
-    private Node root; // root of trie
-    private static class Node
-    {
-        private Object val;
-        private Node[] next = new Node[R];
-    }
-    public Value get(String key)
-    {
-        Node x = get(root, key, 0);
+    private static int R = 256; // radix
+    private Node<K> root; // root of trie
+    
+    public K get(String key) {
+        Node<K> x = get(root, key, 0);
         if (x == null) return null;
-        return (Value) x.val;
+        return (K) x.val;
     }
-    private Node get(Node x, String key, int d)
-    { // Return value associated with key in the subtrie rooted at x.
-        if (x == null) return null;
+    private Node<K> get(Node<K> x, String key, int d) {
+        // Return K associated with key in the subtrie rooted at x.
+        if (x == null) {
+            return null;
+        }
         if (d == key.length()) return x;
         char c = key.charAt(d); // Use dth key char to identify subtrie.
         return get(x.next[c], key, d+1);
     }
-    public void put(String key, Value val)
-    { root = put(root, key, val, 0); }
-    private Node put(Node x, String key, Value val, int d)
-    { // Change value associated with key if in subtrie rooted at x.
-        if (x == null) x = new Node();
+    public void put(String key, K val) {
+        root = put(root, key, val, 0);
+    }
+    
+    private Node<K> put(Node<K> x, String key, K val, int d) {
+        // Change K associated with key if in subtrie rooted at x.
+        if (x == null) {
+            x = new Node<>();
+        }
         if (d == key.length()) { x.val = val; return x; }
         char c = key.charAt(d); // Use dth key char to identify subtrie.
         x.next[c] = put(x.next[c], key, val, d+1);
         return x;
+    }
+    
+    private static class Node<T>
+    {
+        private T val;
+        private Node<T>[] template;
+        private final Node<T>[] next = Arrays.copyOf(template, R);
+    
+        private Node() {
+        }
     }
 }
